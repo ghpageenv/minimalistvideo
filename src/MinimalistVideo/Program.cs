1b824879124c4c86d040297ffb1bc8264349dae7
Jerome Bell <jeromebell@gmail.com>
2024-03-21 20:06:00 -0500

    Rename

--- src/MinimalistVideo/Program.cs
+++ src/MinimalistVideo/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<MudBlazorPages.App>("#app");
+builder.RootComponents.Add<MinimalistVideo.App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
